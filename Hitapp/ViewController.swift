6e122cc27cf55277e3d9fcf7187da9673b4edb37
Aurora Ruelas <karyna.aurora@gmail.com>
2018-04-03 12:18:12 +0200

    ADD: Mapa,Login y permisos

--- Hitapp/ViewController.swift
+++ Hitapp/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-bj bhkmb k,
+    @IBOutlet var txtUser:UITextField?
+    
+    @IBOutlet var txtPassword:UITextField?
+    
+    @IBOutlet var btnLogin:UIButton?
+    
+ 
+    var UserName:String = "Aurora"
+    @IBAction func clickLogin(){
+        print("Usiario Pulso Login")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
